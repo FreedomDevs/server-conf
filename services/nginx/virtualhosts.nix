33f08246d9300274cd953cf293cab98685b90769
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-08-13 18:25:09 +0300

    Добавил админку

--- services/nginx/virtualhosts.nix
+++ services/nginx/virtualhosts.nix
@@ -76,6 +76,19 @@ in {
     };
     extraConfig = "ssl_ech_file /run/agenix/elysia-game-ech_elysiac.fun.pem;";
   };
+  "admin.elysiac.fun" = {
+    listen = defaultListen;
+    onlySSL = true;
+    sslCertificate = "${../../files/certs/elysiac.fun.crt}";
+    sslCertificateKey = "/run/agenix/elysiac.fun.key";
+
+    locations."/" = {
+      root = "/var/www/www-admin";
+      tryFiles = "$uri $uri/ =404";
+      extraConfig = "add_header Cache-Control \"public, max-age=120, stale-if-error=31536000\";";
+    };
+    extraConfig = "ssl_ech_file /run/agenix/elysia-game-ech_elysiac.fun.pem;";
+  };
 
   "resourcepacks.elysiac.fun" = {
     listen = defaultListen;
